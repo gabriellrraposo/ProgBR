fb9885452b93f2b5733e9f6987fbf85e78b9428a
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-08-22 09:21:02 -0300

    Modificando elementos dinamicamente com JS - revisão

---
+++ JavaScript/REVISÕES/modificandoElementos/script.js
@@ -0,0 +1,7 @@
+let p1 = document.getElementById("p1")
+let p2 = document.getElementById("p2")
+let p3 = document.getElementById("p3")
+
+p1.className = "tipo2"
+p2.setAttribute("class", "tipo1")
+p3.setAttribute("class", "tipo3")
