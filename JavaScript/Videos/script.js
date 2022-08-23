28ff8006c5fe02e4b0a2e81102be15112626ed05
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-08-23 10:26:49 -0300

    Atribui funções a cada botão de controle

---
+++ JavaScript/Videos/script.js
@@ -0,0 +1,31 @@
+let video = document.getElementById("video1")
+
+let retroceder = function () {
+    video.currentTime -= 15
+}
+
+let avancar = function () {
+    video.currentTime += 15
+}
+
+let diminuirVel = function () {
+    video.playbackRate -= 0.15
+}
+
+let aumentarVel = function () {
+    video.playbackRate += 0.15
+}
+
+let pause = function () {
+    video.pause()
+}
+
+let stop = function () {
+    video.pause()
+    video.currentTime = 0
+    video.playbackRate = 1
+}
+
+let play = function () {
+    video.play()
+}
