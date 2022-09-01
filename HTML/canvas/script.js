f0c62eaa100837da0bdd94cbbb73f3dadef88055
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-09-01 08:38:16 -0300

    Criando e estilizando um retângulo com canvas

--- HTML/canvas/script.js
+++ HTML/canvas/script.js
@@ -1,8 +1,21 @@
 let tela = document.getElementById('tela');
 let ctx = tela.getContext('2d');
 
-ctx.moveTo(0, 0);
-ctx.lineTo(250, 250);
-ctx.lineTo(500, 0);
-ctx.lineWidth = 6;
-ctx.stroke();
+// ctx.moveTo(0, 0);
+// ctx.lineTo(250, 250);
+// ctx.lineTo(500, 0);
+// ctx.lineWidth = 6;
+// ctx.stroke();
+
+ctx.rect(20, 20, 100, 200)
+
+ctx.fillStyle = "green"
+// ctx.fillRect(20, 20, 100, 200)
+ctx.lineWidth = "15"
+ctx.strokeStyle = "purple";
+// ctx.strokeRect(20, 20, 100, 200)
+
+ctx.fill()
+ctx.stroke()
+
+ctx.clearRect(45, 40, 50, 100)
