e82374567f03040c05b1f33a724e68d1f8c14454
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-09-01 08:55:38 -0300

    Utilizando path para fazer desenhos independentes

--- HTML/canvas/script.js
+++ HTML/canvas/script.js
@@ -7,15 +7,46 @@ let ctx = tela.getContext('2d');
 // ctx.lineWidth = 6;
 // ctx.stroke();
 
-ctx.rect(20, 20, 100, 200)
+// ctx.rect(20, 20, 100, 200)
 
-ctx.fillStyle = "green"
-// ctx.fillRect(20, 20, 100, 200)
-ctx.lineWidth = "15"
-ctx.strokeStyle = "purple";
-// ctx.strokeRect(20, 20, 100, 200)
+// ctx.fillStyle = "green"
+// // ctx.fillRect(20, 20, 100, 200)
+// ctx.lineWidth = "15"
+// ctx.strokeStyle = "purple";
+// // ctx.strokeRect(20, 20, 100, 200)
 
-ctx.fill()
-ctx.stroke()
+// ctx.fill()
+// ctx.stroke()
 
-ctx.clearRect(45, 40, 50, 100)
+// ctx.clearRect(45, 40, 50, 100)
+
+
+//////////////////////////////////
+
+ctx.beginPath()
+
+ctx.lineWidth = 4;
+ctx.strokeStyle = "red";
+
+ctx.moveTo(20, 20);
+ctx.lineTo(200, 100);
+ctx.lineTo(100, 200);
+ctx.closePath();
+ctx.fillStyle = "orange";
+
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+
+ctx.lineWidth = "10";
+ctx.strokeStyle = "violet"
+
+ctx.moveTo(100, 100);
+ctx.lineTo(490, 245);
+ctx.lineTo(300, 400);
+ctx.closePath();
+ctx.fillStyle = "bisque";
+
+ctx.fill();
+ctx.stroke();
