0427f84e518b736ad1c16f0d3f040a621d09cef5
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-09-06 08:04:14 -0300

    Usando Strict mode JS

---
+++ JavaScript/strictmode/script.js
@@ -0,0 +1,11 @@
+"use strict"
+//Variáveis devem ser, obrigatoriamente, declaradas
+
+let imprimir = function() {
+    x = 6
+    console.log(x)
+}
+
+imprimir()
+console.log(window)
+console.log(x)
