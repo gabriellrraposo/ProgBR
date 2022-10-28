c03e12511330e18305a942ee5db0289ab0cdb823
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-10-28 10:20:47 -0300

    Importando firebase para o projeto

---
+++ Firebase/app.js
@@ -0,0 +1,10 @@
+console.log(firebase)
+
+// let db = firebase.firestore()
+
+// db.collection("turmaA").get()
+//                         .then((snapshot) => {
+//                             snapshot.forEach((doc) => {
+//                                 console.log(doc)
+//                             })
+//                         })
