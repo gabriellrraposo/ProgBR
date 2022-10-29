d150c3c49f73e44469c501b0f1531183e87c3e44
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-10-29 12:45:13 -0300

    Adicionando novos usuários por meio do Javascript

--- Firebase/app.js
+++ Firebase/app.js
@@ -14,7 +14,7 @@ let db = firebase.firestore()
 
 turmaA = db.collection('turmaA')
 
-
+// -------- LEITURA DE DADOS DO BD -----------
 // //Lendo as informações contidas dentro da coleção turmaA do banco de dados colégio
 // turmaA
 //     .get()
@@ -78,24 +78,71 @@ turmaA = db.collection('turmaA')
 //     })
 
 
-//Verificando os alunos que tiraram notas acima de 7
-turmaA.where("notas.nota1", ">", 7)
-    .get()
-    .then(snapshot => {
-        snapshot.forEach(doc => {
-            let aluno = doc.data()
-            console.log(aluno.nome)
-        })
-    })
+// //Verificando os alunos que tiraram notas acima de 7
+// turmaA.where("notas.nota1", ">", 7)
+//     .get()
+//     .then(snapshot => {
+//         snapshot.forEach(doc => {
+//             let aluno = doc.data()
+//             console.log(aluno.nome)
+//         })
+//     })
 
 
-//Verificando alunos que tiraram notas de recuperação
-turmaA.where("notas.nota1", "<", 7)
-    .where("notas.nota1", ">", 5)
-    .get()
-    .then(snapshot => {
-        snapshot.forEach(doc => {
-            let aluno = doc.data()
-            console.log(aluno.nome)
-        })
-    })
+// //Verificando alunos que tiraram notas de recuperação
+// turmaA.where("notas.nota1", "<", 7)
+//     .where("notas.nota1", ">", 5)
+//     .get()
+//     .then(snapshot => {
+//         snapshot.forEach(doc => {
+//             let aluno = doc.data()
+//             console.log(aluno.nome)
+//         })
+//     })
+
+// ------------- ADIÇÃO DE DADOS NO BD -------------
+
+// //Adicionando usuário com id aleatório
+// turmaA.add({
+//     nome: 'Gabriel',
+//     sobrenome: 'Raposo',
+//     notas: {
+//         nota1: 7.9,
+//         nota2: 8.8
+//     }
+// }).then(doc => {
+//     console.log('Usuário inserido com sucesso!', doc)
+// }).catch(err => {
+//     console.log(err)
+// })
+
+//Adicionando usuário com id aleatório
+turmaA.add({
+    nome: 'Júlia',
+    sobrenome: 'Andrade',
+    notas: {
+        nota1: 6.4,
+        nota2: 5.8
+    }
+}).then(doc => {
+    console.log('Usuário inserido com sucesso!', doc)
+}).catch(err => {
+    console.log(err)
+})
+
+
+
+turmaA.doc("alunoNovo").set(
+    {
+        nome: 'Mariana',
+        sobrenome: 'Carvalho',
+        notas: {
+            nota1: 7.2,
+            nota2: 9.4
+        }
+    }
+).then(() => {
+    console.log('Usuário inserido com sucesso!')
+}).catch(err => {
+    console.log(err)
+})
