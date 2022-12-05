7045eb09a18831769c2f51613d2331cacb04fdb7
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-12-05 19:32:17 -0300

    Redimensionando imagem utilizando resize e toFile do sharp

--- Node.js/miniProjeto - compressao de imagem/app.js
+++ Node.js/miniProjeto - compressao de imagem/app.js
@@ -1,6 +1,16 @@
+const sharp = require('sharp')
+
 let path = process.argv[2]
 let width = Number(process.argv[3])
 
 let resize = (path, width) => {
+    sharp(path).resize({width: width}).toFile("./temp/output_resize.jpg", (err) => {
+        if (err) throw err
 
+        console.log('Imagem redimensionada com sucesso!')
+    })
+    // resize: redimensiona o tamanho da imagem (poderia ser adicionado o height tbm)
+    // toFile: define para onde a imagem redimensionada irá
 }
+
+resize(path, width)
