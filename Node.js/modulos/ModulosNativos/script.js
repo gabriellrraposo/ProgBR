2f86efb7b26a39e5573fa88f602c6624d678de7d
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-12-05 11:08:39 -0300

    Apaga arquivo utilizando unlink do Node

--- Node.js/modulos/ModulosNativos/script.js
+++ Node.js/modulos/ModulosNativos/script.js
@@ -1,8 +1,20 @@
 let fs = require('fs')
 
-fs.appendFile('text.txt', 'Olá Mundo!', (err) => {
+// fs.writeFile('text.txt', 'Hello World!', (err) => {
+//     if (err) throw err
+
+//     console.log('Arquivo criado com sucesso!')
+// })
+
+// fs.appendFile('text.txt', 'Olá Mundo!', (err) => {
+//     if (err) throw err
+
+//     console.log('Arquivo atualizado com sucesso!')
+// })
+
+fs.unlink('text.txt', (err) => {
     if (err) throw err
 
-    console.log('Arquivo atualizado com sucesso!')
+    console.log('Arquivo apagado com sucesso!')
 })
 
