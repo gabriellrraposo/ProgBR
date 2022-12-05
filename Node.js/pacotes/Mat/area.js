2e7b1c9a1d7ecd4944f5d83ada7b7e081edf1f55
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-12-05 19:08:47 -0300

    Realiza operações utilizando funções presentes nos módulos do pacote

--- Node.js/pacotes/Mat/area.js
+++ Node.js/pacotes/Mat/area.js
@@ -1,2 +1,2 @@
 module.exports.area_quadrado = function quadrado(lado) {return lado * lado}
-module.exports.area_retangulo = function quadrado(base, altura) {return base * altura}
+module.exports.area_retangulo = function retangulo(base, altura) {return base * altura}
---
+++ Node.js/pacotes/meuPrograma/index.js
@@ -0,0 +1,6 @@
+let mat = require('matematica')
+
+console.log(mat.adicao(2, 2))
+console.log(mat.subtracao(4, 2))
+console.log(mat.area_quadrado(2))
+console.log(mat.area_retangulo(2, 8))
