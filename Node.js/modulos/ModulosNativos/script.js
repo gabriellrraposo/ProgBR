8868dcfb49801b131d292259e6bb761985eb3e50
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-12-05 11:15:58 -0300

    Lendo um arquivo com o readFile no formato UTF-8

--- Node.js/modulos/ModulosNativos/script.js
+++ Node.js/modulos/ModulosNativos/script.js
@@ -1,20 +1,33 @@
 let fs = require('fs')
 
-// fs.writeFile('text.txt', 'Hello World!', (err) => {
+// fs.writeFile('arquivoTeste.txt', 'Hello World!', (err) => {
 //     if (err) throw err
 
 //     console.log('Arquivo criado com sucesso!')
 // })
 
-// fs.appendFile('text.txt', 'Olá Mundo!', (err) => {
+// fs.appendFile('arquivoTeste.txt', 'Texto para arquivo teste', (err) => {
 //     if (err) throw err
 
 //     console.log('Arquivo atualizado com sucesso!')
 // })
 
-fs.unlink('text.txt', (err) => {
+// fs.unlink('text.txt', (err) => {
+//     if (err) throw err
+
+//     console.log('Arquivo apagado com sucesso!')
+// })
+
+// fs.rename('arquivoTeste.txt', 'novoNome.txt', (err) => {
+//     if (err) throw err
+
+//     console.log('Arquivo renomeado com sucesso!')
+// })
+
+fs.readFile('novoNome.txt', 'UTF8', (err, fd ) => {
     if (err) throw err
 
-    console.log('Arquivo apagado com sucesso!')
+    console.log(fd)
 })
+//É preciso passar o UTF8 para que seja imprimido o texto normalmente, senão imprimiria um Buffer
 
