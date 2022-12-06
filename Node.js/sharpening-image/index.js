9690065655ec291c9288711dc1f596d712005e91
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-12-06 14:57:27 -0300

    Gera contador de quantas imagens existem, para nomeá-las corretamente e ajusta os presets de nitidez da imagem

--- Node.js/sharpening-image/index.js
+++ Node.js/sharpening-image/index.js
@@ -1,9 +1,25 @@
 const sharp = require('sharp')
+const fs = require('fs')
 
+const length = fs.readdirSync('./processed_images').length
 const sharpenImage = () => {
+    
     sharp('./images/skyline-image.jpg')
-    .sharpen({sigma: 13})
-    .toFile(__dirname + '/processed_images/sharpen_skyline-image.jpg')
+    .sharpen(
+        {
+            sigma: 1,
+            m1: 1,
+            m2: 0,
+            x1: 2,
+            y2: 10,
+            y3: 20
+        }
+        )
+    .toFile(__dirname + `/processed_images/sharpen_skyline-image` + `(${length + 1}).jpg`, err => {
+        if (err) throw err
+
+        console.log("Imagem tornou-se mais nítida com sucesso!")
+    })
 }
 
 sharpenImage()
