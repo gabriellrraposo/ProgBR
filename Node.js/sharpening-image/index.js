7af458e851a8fd46176f89a57d0fad91f2c961ff
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-12-06 11:10:15 -0300

    Cria pasta para as imagens

--- Node.js/sharpening-image/index.js
+++ Node.js/sharpening-image/index.js
@@ -1,7 +1,7 @@
 const sharp = require('sharp')
 
 const sharpenImage = () => {
-    sharp('./skyline-image.jpg')
+    sharp('./images/skyline-image.jpg')
     .sharpen({sigma: 13})
     .toFile(__dirname + '/processed_images/sharpen_skyline-image.jpg')
 }
