49886e142a2ef183b6f9b501a817f5a5a7aa4d89
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-12-07 15:23:51 -0300

    Enviando uma mensagem caso seja um GET

--- Node.js/Express/index.js
+++ Node.js/Express/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
-const PORT = 5000
 
+app.use("/meusite", express.static(path.join(__dirname, 'client')))
+// Sempre procura pelo arquivo index.html
+
+app.get("/", (req, res) => {
+    // res.set({"Content-Type": "text/plain"})
+
+    res.type('txt')
+    res.send('<h1>hello world from GET</h1>')
+})
+
+const PORT = 5000
 app.listen(PORT, () => {
     console.log(`Server Running on port: ${PORT}`)
 })
