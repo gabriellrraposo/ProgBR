c6357d4842cd092a1e7c49fd633702cc1fecb616
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-12-13 11:56:10 -0300

    Cria função que substitui textos específicos pelos seus devidos valores do banco de dados

--- Template Engine/criando template/index.js
+++ Template Engine/criando template/index.js
@@ -5,15 +5,23 @@ const app = express()
 
 let user = {
     id: 0,
-    name: 'Igor',
+    name: 'Igor Oliverira',
     phone: '(222) 222-4325'
+}
+
+let render = (data, obj) => {
+    for (let key in obj) {
+        data = data.replace(`{{{${key}}}}`, obj[key]) //Substitui o '{{{key}}}' do HTML pelo seu valor real
+    }
+
+    return data
 }
 
 app.get("/", (req, res) => {
 
     fs.readFile('./templates/user.html', 'UTF8', (err, fd) => { //Lê o arquivo determinado
         if (!err) {
-            res.send(fd) //Manda uma resposata/dado ao usuário após ele fazer uma requisição. Nesse caso, foi o arquivo html que foi lido acima.
+            res.send(render(fd, user)) //Manda uma resposata/dado ao usuário após ele fazer uma requisição. Nesse caso, foi o arquivo html que foi lido acima.
         }
     })
 })
